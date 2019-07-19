250f3e7f94377eb93ee291bbcc19e43c2a8893b2
Andrew Hollabaugh <andrewhollabaugh12@gmail.com>
2019-07-18 21:50:33 -0400

    untested prox code

--- include/prox.h
+++ include/prox.h
@@ -4,7 +4,7 @@
 #include <avr/io.h>
 #include <stdio.h>
 
-#define PROX_VCNL_ADDR 0x13
+#define PROX_TWI_ADDR 0x13
 
 //VCNL4010 registers
 #define PROX_COMMAND_REG 0x80
@@ -13,6 +13,11 @@
 #define PROX_PROXDATA_REG 0x87
 
 #define PROX_IRLED_CURRENT 20 //200mA
+#define PROX_PROXRATE_16_625 3
+
+//command register values
+#define PROX_MEASUREPROXIMITY 0x08
+#define PROX_PROXREADY 0x20
 
 void prox_init(void);
 uint16_t prox_get_data(void);
--- src/prox.c
+++ src/prox.c
@@ -6,20 +6,22 @@ void prox_init()
     twi_init();
 
     //set IR LED current
-    twi_write_reg(PROX_VCNL_ADDR, PROX_IRLED_REG, PROX_IRLED_CURRENT);
+    twi_write_reg(PROX_TWI_ADDR, PROX_IRLED_REG, PROX_IRLED_CURRENT);
+
+    //set prox reading rate
+    twi_write_reg(PROX_TWI_ADDR, PROX_PROXRATE_REG, PROX_PROXRATE_16_625);
 }
 
 uint16_t prox_get_data()
 {
-    prox_TWI_start();
-    prox_TWI_write_init();
-    prox_TWI_write_data(PROX_MPU6050_PROX_XOUT_H); //write address of XOUT_H register
-    prox_TWI_start();
-    prox_TWI_read_init();
-    uint16_t prox_data = prox_TWI_read_data() << 8; //read upper byte of data from XOUT and shift
-    prox_data |= prox_TWI_read_data(); //OR prox_data with lower byte from XOUT, address automatically increments
-    prox_TWI_stop();
-    //printf("PROX: %i\n", prox_data);
-    return prox_data;
+    //write command register to proximity mode
+    twi_write_reg(PROX_TWI_ADDR, PROX_COMMAND_REG, PROX_MEASUREPROXIMITY);
+
+    while(1)
+    {
+        uint8_t result = twi_read_reg(PROX_VCNL_ADDR, PROX_COMMAND_REG);
+        if(result & PROX_PROXREADY)
+            return twi_read_reg16(PROX_TWI_ADDR, PROX_PROXDATA_REG);
+    }
 }
 
