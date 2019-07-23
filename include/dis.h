67ab656699b8d164ecab124a8b16a207438da2d1
Andrew Hollabaugh <andrewhollabaugh12@gmail.com>
2019-07-22 22:07:59 -0400

    added values to dis.h for ir

--- include/dis.h
+++ include/dis.h
@@ -10,11 +10,15 @@
 #define DIS_HOME_SPEED 30
 #define DIS_DIR 0
 #define DIS_HOME_DIR 1
+#define DIS_IR_BROKEN 0
+#define DIS_IR_UNBROKEN 4
+#define DIS_IR_STABLE_NUM 100
 
 void dis_init(void);
 void dis_dispense_init(uint8_t num_items);
 void dis_dispense_no_home_init(uint8_t num_items);
 void dis_home_init(void);
+uint8_t dis_ir_get(void);
 void dis_wait_for_dispense(void);
 void dis_wait_for_stable(void);
 void dis_done(void);
